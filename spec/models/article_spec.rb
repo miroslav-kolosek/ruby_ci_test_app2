0a0c71aaea7c3aa80890ca313ff13b0946998aae
miroslav <miroslav@kolosek.com>
2023-03-21 10:20:27 +0100

    One article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Article, type: :model do
   end
 
   it do
-    expect(Article.count).to be 0
+    expect(Article.count).to eq 0
+  end
+
+  context "with articles" do
+    it do
+      Article.create(title: 'article 1', body: 'article 1', status: 'public')
+      expect(Article.count).to eq 1
+    end
   end
 end
